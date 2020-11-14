31695ca95182a2510b6436060ab16c05f6bd8f02
Amir_Sardar_Abadi <41546074+Amir-Sardar-Abadi@users.noreply.github.com>
2020-11-14 23:52:47 +0330

    commit on master

---
+++ reading-files-httpReq/addDataToXmlArtists.php
@@ -0,0 +1,13 @@
+<?php 
+
+$songName = $_POST['song-name'];
+$urlName = $_POST['url-name'];
+$youtubeName = $_POST['youtube-name'];
+
+echo $songName;
+// $myXmlFile = fopen('artists.xml', 'a');
+
+// $text = ''
+
+
+?>
