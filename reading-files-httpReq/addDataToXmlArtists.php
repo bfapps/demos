ce75373d4811a20d23621601068e14895bc38df8
Amir_Sardar_Abadi <41546074+Amir-Sardar-Abadi@users.noreply.github.com>
2020-11-15 18:26:10 +0330

    commit on master

--- reading-files-httpReq/addDataToXmlArtists.php
+++ reading-files-httpReq/addDataToXmlArtists.php
@@ -5,9 +5,10 @@ $songTagName = $_POST['song-tag-name'];
 $urlName = $_POST['url-name'];
 $youtubeName = $_POST['youtube-name'];
 
-$myXmlFile = fopen('artists.xml', 'a');
 
-$text = '<' . $songTagName . '> ' . '<song-name>' . $songName . "</song-name> <url>" . $urlName . "</url> <youtube>" . $youtubeName . '</youtube>' . '</' . $songTagName . '>';
+
+$myXmlFile = fopen('artists.xml', 'a');
+$text = "\n\t<" . $songTagName . ">\n\t\t<song-name>\n\t\t\t" . $songName . "\n\t\t</song-name>\n\t\t<url>\n\t\t\t" . $urlName . "\n\t\t</url>\n\t\t<youtube>\n\t\t\t" . $youtubeName . "\n\t\t</youtube>\n\t</" . $songTagName . ">\n";
 
 fwrite($myXmlFile, $text);
 fclose($myXmlFile);
