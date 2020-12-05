9d9ee9b324f3a91e9747dd10873f1f7bf6e1e165
BFApps <41546074+Amir-Sardar-Abadi@users.noreply.github.com>
2020-12-06 00:10:28 +0330

    commit on master

--- experiment/article.js
+++ experiment/article.js
@@ -11,10 +11,11 @@ function fetchData (url, cFunction) {
 
 window.addEventListener("load", function () {
     fetchData("article.json", loadData);
+    addListener();
 });
 
 function loadData (data) {
-    let secLength = data.length;
+    articleSec.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
         let article = document.createElement('article');
         let img = document.createElement('img');
@@ -26,13 +27,21 @@ function loadData (data) {
         img.setAttribute('loading', "lazy");
         img.setAttribute('alt', 'article image');
         h2.innerHTML = data[i].title;
-        h4.innerHTML = data[i].date;
+        if (data[i].date.length > 8) {
+            h4.innerHTML = data[i].date;
+        }
+        else if (data[i].date.length < 8) {
+            h4.innerHTML = "Once Upon A Time ...";
+        }
+        
         a.setAttribute('href', 'open-article.html');
         a.innerHTML = "Read More";
         let adress = "article-" + i + ".json";
         fetchData(adress, loadTxt);
         p.innerHTML = localStorage.getItem('txt').slice(0, 150) + "...";
-        article.appendChild(img);
+        if (data[i].src.length > 4) {
+            article.appendChild(img);
+        }
         article.appendChild(h2);
         article.appendChild(h4);
         article.appendChild(p);
@@ -45,4 +54,15 @@ function loadTxt (data) {
     let txt = data[0].txt;
     localStorage.setItem('txt', txt); 
 }
+function addListener () {
+    document.querySelectorAll('#article-sec article').forEach(function (item) {
+        item.addEventListener("click", function (event) {
+            alert("bad");
+
+            // let src = event.target.parentNode.children[0].src;
+            // console.log(src);
+        })
+    })
+}
+
 
